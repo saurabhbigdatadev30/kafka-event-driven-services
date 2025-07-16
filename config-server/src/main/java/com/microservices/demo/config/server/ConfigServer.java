b3926b91cbc78a81c5a399f5b000102dbb21c851
saurabhbigdatadev30 <saurabhbigdatadev30@gmail.com>
2025-07-16 17:26:45 +0530

    Test change is committed

--- config-server/src/main/java/com/microservices/demo/config/server/ConfigServer.java
+++ config-server/src/main/java/com/microservices/demo/config/server/ConfigServer.java
@@ -4,7 +4,7 @@ package com.microservices.demo.config.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-
+//Test
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigServer {
